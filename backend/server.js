c2607f5d91d4c4e738bc4c51071219a59f12af92
Vihaan Philip <viha0001@student.monash.edu>
2023-07-17 17:39:56 +0800

    create middleware and routes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,18 @@ const express = require('express');
 // express app
 const app = express();
 
+// middleware & static files
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
 // routes
 app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the app' });
+    res.json({ msg: 'Welcome to the app haha' });
 });
 
 // listen for requests
-app.listen(process.env, () => { 
-    console.log('listening for requests on port 4000!!!')
+app.listen(process.env.PORT, () => { 
+    console.log('listening for requests on port', process.env.PORT)
 });
