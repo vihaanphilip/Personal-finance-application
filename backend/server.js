95d6b242d447f3b95d54077e6a891048ab7cc572
Vihaan Philip <viha0001@student.monash.edu>
2023-07-17 19:45:11 +0800

    post, patch and delete handlers created

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
 
+
 // middleware & static files
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the app haha' });
-});
+// app.get('/', (req, res) => {
+//     res.json({ msg: 'Welcome to the app haha' });
+// });
+app.use('/api/workouts', workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => { 
