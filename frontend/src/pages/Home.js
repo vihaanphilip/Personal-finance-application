941aa553bea3860a5308472e55ece93d7c260d78
Vihaan Philip <viha0001@student.monash.edu>
2023-07-21 15:53:34 +0800

    Update key for workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('http://localhost:3000/api/workouts');
+            const json = await response.json();
+            // console.log(json);
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        }
+
+        fetchWorkouts()
+    }, []);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}
+                    </p>
+                ))}
+            </div>
         </div>
     )
 }
