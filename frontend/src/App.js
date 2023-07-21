142922a879fb904345fe2538d8e35436ef7f569a
Vihaan Philip <viha0001@student.monash.edu>
2023-07-21 13:19:56 +0800

    fixed react issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages & components
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Home = () => {
     return (
         <div className="home">
@@ -6,4 +8,4 @@ const Home = () => {
     )
 }
 
-export default App;
+export default Home;
