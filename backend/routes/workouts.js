582d554cfe9a0f17b22ca0336b5679e54e8ac222
Vihaan Philip <viha0001@student.monash.edu>
2023-07-21 11:09:19 +0800

    [Backend] created delete and update controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -5,6 +5,8 @@ const {
     getWorkouts,
     getWorkout,
     createWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router();
@@ -19,13 +21,9 @@ router.get('/:id', getWorkout);
 router.post('/', createWorkout);
 
 // Delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({ msg: 'Delete a workout' });
-});``
+router.delete('/:id', deleteWorkout);
 
 // Update a workout
-router.patch('/:id', (req, res) => {
-    res.json({ msg: 'Update a workout' });
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
