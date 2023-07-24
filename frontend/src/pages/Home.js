69ebaed12348c80ebba9e64a29bfdbf0f269a5e3
Vihaan Philip <viha0001@student.monash.edu>
2023-07-24 12:48:16 +0800

    create workout form interface

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null);
@@ -27,6 +29,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
